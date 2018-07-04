f0d11faef1b5a6421930d616a0410e7970cc2043
User-PC\User <cyorchid@gmail.com>
2018-07-04 15:43:16 +0900

    업데이트 주기 변경

--- projects/Timer.jsx
+++ projects/Timer.jsx
@@ -3,26 +3,63 @@ import moment from 'moment';
 import 'moment/locale/ko';
 moment.locale('ko');
 
+const TIME_FORMAT = 'YYYY-MM-DD A hh:mm';
 class Timer extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
-    this.state.date = moment();
+    this.state = {
+      date: moment(),
+    };
 
-    this.nTimer = setInterval(this.onTick, 1000);
+    this.nTimer = setInterval(() => {
+      this.setState({
+        date: moment(),
+      });
+    }, 1000);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      this.state.date.format(TIME_FORMAT) === nextState.date.format(TIME_FORMAT)
+    )
+      return false;
+    return true;
   }
 
   onTick = () => {
     this.forceUpdate();
-    console.log(moment('2018-07-04T17:00:00+09:00').valueOf());
+  };
+
+  timeChecking = () => {
+    const Time = moment(this.props.expireDate).valueOf() - moment().valueOf();
+    return this.msToTime(Time);
+  };
+
+  msToTime = s => {
+    const ms = s % 1000;
+    s = (s - ms) / 1000;
+    const secs = s % 60;
+    s = (s - secs) / 60;
+    const mins = s % 60;
+    const hrs = (s - mins) / 60;
+
+    return hrs + ':' + mins + ':' + secs;
   };
 
   render() {
+    const { expireDate } = this.props;
+    const { date } = this.state;
+
+    if (moment(this.props.expireDate) < this.state.date) {
+      return <div>종료 되었습니다.</div>;
+    }
+
     return (
       <div>
-        <div>현재시간: {moment().format('YYYY-MM-DD A hh:mm:ss')} </div>
-        <div>{moment('2018-07-04T17:00:00+09:00').fromNow()} 남았습니다.</div>
+        <div>현재시간: {date.format(TIME_FORMAT)} </div>
+        <div>{moment(expireDate).fromNow()}에 수업이 종료됩니다.</div>
+        <div>{this.timeChecking()} 남았습니다.</div>
       </div>
     );
   }
